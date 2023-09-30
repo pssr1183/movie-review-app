b6702297d9cca3135a91214930e5552cd75785db
pssr1183 <poornasaidarla@gmail.com>
2023-09-30 21:25:47 +0530

    added sentiment response

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
 from flask import Flask, render_template, request
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import pandas as pd
-import numpy as np
 import tensorflow as tf
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense, GlobalAveragePooling1D
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
 import numpy as np
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Embedding
+
 
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-# Load the sentiment analysis model
-# Tokenizer for text preprocessing
-
 @app.route('/predict', methods=['POST'])
 def predict():
+    
     # Get the movie review from the form
+    request_data = request.get_json()
+    reviews = request_data.get('reviews')
+    print(reviews)
+    reviews = [review.strip() for review in reviews if review.strip()]
+    if not reviews:
+        return jsonify({'error': 'Please provide one or more valid movie reviews.'}), 400
     max_words = 10000
     tokenizer = Tokenizer(num_words=max_words, oov_token="<OOV>")
     max_sequence_length = 200
     model= tf.keras.models.load_model("mymodel.h5")
-    from tensorflow.keras.preprocessing.text import tokenizer_from_json
     with open('tokenizer_config.json', 'r', encoding='utf-8') as json_file:
         loaded_tokenizer_json = json_file.read()
 
@@ -37,9 +34,6 @@ def predict():
 # Use the loaded tokenizer for tokenization
     # Threshold to classify reviews as positive or negative
     threshold = 0.4
-    request_data = request.get_json()
-    reviews = request_data.get('reviews')
-    print(reviews)
 # Predict sentiment for the movie reviews with negation handling
     movie_sequences = loaded_tokenizer.texts_to_sequences(reviews)
     movie_padded = pad_sequences(movie_sequences, maxlen=max_sequence_length, padding='post', truncating='post')
@@ -61,7 +55,11 @@ def predict():
         print(f"Review: {review}\nSentiment: {sentiment_label}\n")
     print("Average Sentiment:", average_sentiment)
     print("Rating:", rating, "/5")
-    response_data = {'rating': rating}
+    response_data = {
+        'rating': rating,
+        'sentiment':sentiment_label
+                     
+                     }
     return jsonify(response_data)
     
 
